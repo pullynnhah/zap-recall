17ed5c564b43a9e4290c58f5c1be287755484b00
Paula Rodrigues <pullynnhah@icloud.com>
2023-04-20 21:24:25 -0300

    :tada: create setup repo for project

---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+export const COLORS = {
+  grey: "#333",
+  green: "#2FBE34",
+  orange: "#FF922E",
+  red: "#FF3030"
+};
---
+++ src/constants/deck.js
@@ -0,0 +1,31 @@
+export const DECK = [
+  {
+    question: "Como iniciar um repositório git?",
+    answer: "git init"
+  },
+  {
+    question:
+      "Em qual arquivo deve-se colocar o nome de pastas/arquivos a serem ignorados pelo git?",
+    answer: ".gitignore"
+  },
+  {
+    question: "Quais os passos para adicionar mudanças ao repo no GitHub?",
+    answer: "git add <filename><br />git commit -m 'mensagem'<br />git push"
+  },
+  {
+    question: "Qual o comando para adicionar um arquivo a área de staging?",
+    answer: "git add <filename>"
+  },
+  {
+    question: "Qual o commando para commitar as mudanças?",
+    answer: "git commit -m 'mensagem'"
+  },
+  {
+    question: "Como ver o histórico de commits?",
+    answer: "git log"
+  },
+  {
+    question: "Como ver a área de staging?",
+    answer: "git status"
+  }
+];
